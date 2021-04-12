b082cfcd0091bf662fa740f68054e7be6441c8f0
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2021-04-12 11:08:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// const paintWall = function () {
+//     console.log("The wall has been painted red")
+// };
+
+//     paintWall();
+
+
+// const paintWall = function (color) {
+//     console.log("The wall has been painted " + color);
+// };
+
+// const colorOne = "green";
+// const colorTwo = "blue";
+
+// paintWall(colorOne);
+// paintWall(colorTwo);
+
+
+const paintWall = function(wall, color) {
+    console.log(wall + " wall has been painted " + color);
+}
+
+const wallOne = "north";
+const wallTwo = "east";
+const wallThree = "south";
+const wallFour = "west";
+
+const colorOne = "green";
+const colorTwo = "blue";
+
+paintWall(wallOne, colorOne);
+paintWall(wallTwo, colorTwo);
